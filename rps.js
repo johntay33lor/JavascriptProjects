a99be00cd3312844ef60e9b6fe4726cb71c7ca9a
John <johntay33lor@gmail.com>
2023-04-07 22:48:26 -0400

    rps

--- rps.js
+++ rps.js
@@ -8,9 +8,13 @@ function getHand() {
 // Define two objects for two players. Each player has name and getHand() properties.
 const player1 = { name: 'Player 1', getHand: getHand };
 const player2 = { name: 'Player 2', getHand: getHand };
+const player3 = { name: 'Player 3', getHand: getHand };
+const player4 = { name: 'Player 4', getHand: getHand };
 let playUntil = 3
 let player1Score = 0
 let player2Score = 0
+let player3Score = 0
+let player4Score = 0
 
 // Define a function called playRound() that
 // Takes two player objects as arguments
@@ -84,9 +88,10 @@ function playRound(player1, player2) {
 //playRound(player1, player2);
 
 function playGame(player1, player2, playUntil) {
-    while (playUntil > player1Score &&  playUntil > player2Score ) {
+
+    while (playUntil > player1Score && playUntil > player2Score) {
         playRound(player1, player2)
-        console.log("Player1 Score " + player1Score + " Player2 Score "  + player2Score);
+        console.log("Player1 Score " + player1Score + " Player2 Score " + player2Score);
     }
 
     if (player1Score > player2Score) {
@@ -99,8 +104,34 @@ function playGame(player1, player2, playUntil) {
     }
 };
 
-playGame(player1, player2, playUntil);
+//playGame(player1, player2, playUntil);
 console.log()
+
+function playTournament(player1, player2, player3, player4, playUntil) {
+    playGame(player1, player2, playUntil);
+    playGame(player3, player4, playUntil);
+    if (player1Score > player2Score && player3Score > player4Score) {
+        playGame(player1, player3, playUntil);
+        if (player1Score > player3Score) {
+            console.log(player1.name + " is the world champion! ");
+        }
+        else (player1Score < player3Score)
+        console.log(player3.name + " is the world champion! ");
+
+    }
+    else (player2Score > player1Score && player4Score > player3Score); {
+        playGame(player2, player4, playUntil);
+        if (player2Score > player4Score) {
+            console.log(player2.name + " is the world champion! ");
+        }
+        else (player2Score < player4Score); {
+            console.log(player4.name + " is the world champion! ");
+        }
+    }
+   
+
+}
+playTournament(player1, player2, player3, player4, playUntil);
 
 
 
